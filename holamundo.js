f79e19791c70e5081552d33c8ef34e200425803a
lareaza-web <larezaweb@gmail.com>
2020-05-18 13:57:05 -0300

    holamundo

---
+++ holamundo.js
@@ -0,0 +1,5 @@
+alert("Hola mundo desde Archivo externo");
+alert("Bienvenido a mi script 1");
+document.write("Inserto texto luego de las alerts");
+console.log("muestra esto en la pantalla");
+console.log(20+80);
